0d77a040e869b0f83644c4b204f4c662a5d33282
Rostislav Otevřel <rostislav@otevrel.net>
2023-01-13 19:37:09 +0100

    Create new directiry and program. Also created .gitignore.

---
+++ comparator/program.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    printf("\033[0;31m----- COMPARATOR -----\n\033[0;37mThis console app takes two numbers as input and outputs which one is greater.\nYou can enter only whole numbers in range from 0 to 100, otherwise the app won't work.");
+    
+}
