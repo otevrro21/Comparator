c2d929471eb934c4b1243064ee82357851cae874
Rostislav Otevřel <rostislav@otevrel.net>
2023-01-14 13:16:02 +0100

    linter test

--- comparator/program.c
+++ comparator/program.c
@@ -40,5 +40,7 @@ void num_input(){
 
 int main(){
     printf("\033[0;31m----- COMPARATOR -----\n\033[0;37mThis console app takes two numbers as input and outputs which one is greater.\nYou can enter only whole numbers in range from 0 to 100, otherwise the app won't work.\n");
-    num_input();
+        num_input();
+////hello 
+test
 }
